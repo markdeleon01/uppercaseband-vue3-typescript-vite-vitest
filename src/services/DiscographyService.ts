424a85f3ab24135039d05d4e73a344847f2f42aa
Mark de Leon <mark_deleon@hotmail.com>
2024-07-05 01:03:11 -0400

    Refactored services and typescript configuration

--- src/services/DiscographyService.ts
+++ src/services/DiscographyService.ts
@@ -1,14 +1,4 @@
-import axios from 'axios'
-
-const apiClient = axios.create({
-  baseURL: `https://my-json-server.typicode.com/markdeleon01/uppercaseband-vue3-ts-vite`,
-  withCredentials: false, // This is the default
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  },
-  timeout: 10000 // throw error if API call takes longer than 10 seconds
-})
+import apiClient from '@/utils/apiClient'
 
 export default {
   getReleases() {
